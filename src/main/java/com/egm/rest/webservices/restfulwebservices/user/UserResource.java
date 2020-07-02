0eb32661c396a3d1568e49b5b3cc5b75417b7848
Erick GM <m.erickgualpa@gmail.com>
2020-07-02 12:35:41 +0200

    ADD: Internationalization for RESTful Services

--- src/main/java/com/egm/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/egm/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,6 +3,7 @@ package com.egm.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 
 import java.util.List;
+import java.util.Locale;
 
 import javax.validation.Valid;
 
@@ -13,18 +14,23 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
-	
+
+	@Autowired
+	private MessageSource messageSource;
+
 	@Autowired
 	private UserDaoService service;
 	
@@ -82,6 +88,11 @@ public class UserResource {
 			throw new UserNotFoundException("id-"+ id);
 		}
 	
+	}
+	
+	@GetMapping(path = "/hello-world-i18n")
+	public String helloWorldi18n(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 
 }
