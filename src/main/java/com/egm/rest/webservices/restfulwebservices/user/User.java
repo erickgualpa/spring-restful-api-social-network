6e5016dca414f554f987cd6b46583287e9e463ce
Erick GM <m.erickgualpa@gmail.com>
2020-07-09 10:20:24 +0200

    ADD: Post Entity and Many to One Relationship with User Entity

--- src/main/java/com/egm/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/egm/rest/webservices/restfulwebservices/user/User.java
@@ -1,12 +1,16 @@
 package com.egm.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
+
+import com.egm.rest.webservices.restfulwebservices.post.Post;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -26,6 +30,9 @@ public class User {
 	@Past
 	@ApiModelProperty(notes="Birth date should be in the past")
 	private Date birthDate;
+	
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
 	
 	public User() {
 		
@@ -62,6 +69,14 @@ public class User {
 		this.birthDate = birthDate;
 	}
 	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
